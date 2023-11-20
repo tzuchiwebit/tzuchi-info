47620855f60ea44478a0520f713c22eaf787a9da
garry.chiao <garry.chiao@full-stack-artisan.com>
2023-11-20 21:34:34 +0700

    chore: project set to tailwindcss

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,15 @@
-import { Inter } from 'next/font/google'
-const inter = Inter({ subsets: ['latin'] })
-
+// These styles apply to every route in the application
+import './globals.css'
+ 
 export const metadata = {
   title: '慈濟資訊網',
   description: '慈濟資訊網',
 }
-
+ 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,3 @@
-'use client';
-// import Image from 'next/image'
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-
-export default function Home() {
-  return (
-    <main>
-      <Container>
-        <Box>
-          <Card>
-            <Typography variant="h2">Hello World ~</Typography>
-          </Card>
-        </Box>
-      </Container>
-    </main>
-  )
+export default function Page() {
+  return <h1 className="text-3xl font-bold underline">Hello, Next.js!</h1>
 }
