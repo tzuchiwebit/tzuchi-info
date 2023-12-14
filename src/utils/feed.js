c653249f6ebd24c9236fe308c6a448d90f3edf91
garry.chiao <garry.chiao@full-stack-artisan.com>
2023-12-15 00:50:24 +0800

    feat(rss): feed item added

--- src/utils/feed.js
+++ src/utils/feed.js
@@ -2,6 +2,37 @@ import { Feed } from 'feed';
 import fs from 'fs';
 // import config from '../config';
 
+const posts = [
+    {
+        title: '慈濟資訊網',
+        id: 'https://comm2.tzuchi-org.tw/',
+        link: 'https://comm2.tzuchi-org.tw/',
+        url: 'https://comm2.tzuchi-org.tw/',
+        description: 'descriptions 慈濟資訊網 ',
+        content: 'contents 慈濟資訊網',
+        image: 'https://picsum.photos/200',
+        date: new Date(),
+    },
+    {
+        title: 'Title',
+        id: process.env.SITE_URL,
+        link: process.env.SITE_URL,
+        description: 'descriptions ',
+        content: 'contents',
+        image: 'https://picsum.photos/200',
+        date: new Date(),
+    },
+    {
+        title: 'Title',
+        id: process.env.SITE_URL,
+        link: process.env.SITE_URL,
+        description: 'descriptions ',
+        content: 'contents',
+        image: 'https://picsum.photos/200',
+        date: new Date(),
+    },
+]
+
 export const generateRSS = async () => {
     const feed = new Feed({
         title: process.env.TITLE,
@@ -16,13 +47,48 @@ export const generateRSS = async () => {
         },
     });
 
+    posts.forEach(post => {
+        feed.addItem({
+            title: post.title,
+            id: post.url,
+            link: post.url,
+            description: post.description,
+            content: post.content,
+            author: [
+                {
+                    name: "Garry",
+                    email: "garry@example.com",
+                    link: "https://example.com/garry"
+                },
+            ],
+            contributor: [
+                {
+                    name: "Bruce",
+                    email: "bruce@example.com",
+                    link: "https://example.com/bruce"
+                },
+            ],
+            date: post.date,
+            image: post.image
+        });
+    });
+
+    feed.addCategory("Technologie");
+
+    feed.addContributor({
+        name: "Johan Cruyff",
+        email: "johancruyff@example.com",
+        link: "https://example.com/johancruyff"
+      });
+
+
     const invalidCharInXMLSpecRegexp =
         // eslint-disable-next-line no-control-regex
         /((?:[\0-\x08\x0B\f\x0E-\x1F\uFFFD\uFFFE\uFFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF]))/g;
 
     fs.writeFileSync(
         './public/rss.xml',
-        feed.atom1().replace(invalidCharInXMLSpecRegexp, '')
+        feed.rss2().replace(invalidCharInXMLSpecRegexp, '')
     );
     fs.writeFileSync(
         './public/atom.xml',
