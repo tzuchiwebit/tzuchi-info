e7c2b6350df282d8e3af87445f0f4bcba1e7d17e
garry.chiao <garry.chiao@full-stack-artisan.com>
2024-03-10 00:17:04 +0800

    feat(home): widget collapse set

--- src/components/home/widget/Widgets.js
+++ src/components/home/widget/Widgets.js
@@ -5,15 +5,53 @@ import Calendar from "./Calendar"
 import Reminder from "./Reminder"
 import Journel from "./Journal"
 import Thinking from "./Thinking"
+import { Transition } from '@headlessui/react'
+import { useState } from "react"
+import Icon from "@/shared/Icon"
 
 export default function Widgets() {
 
-    return <Container>
-        <Calendar />
-        <Reminder />
-        <Journel />
-        <Thinking />
-    </Container>
+    const [open, setOpen] = useState(false);
+
+    const toggle = () => {
+        setOpen(!open);
+    }
+
+    return <div className="w-full">
+        <div className="w-full border-b border-solid border-gray-gray7 justify-between pb-2 mb-2 hidden laptop:flex desktop:hidden">
+            <div className="font-bold text-lg text-primary-blue1">
+                {open ? '' : '志工早會 • 證嚴上人每日一叮嚀 • 慈濟週報 • 宗門學思'}
+            </div>
+            <div
+                className="font-medium text-lg text-primary-blue1 flex gap-1 cursor-pointer"
+                onClick={toggle}>
+                {open ? '收合' : '展開'} <Icon.DownArrow style={{ width: 20, transform: open ? 'rotate(180deg)' : '', transition: 'all .3s' }} />
+            </div>
+        </div>
+        <Transition
+            show={open}
+            className="transition-all duration-300 overflow-hidden hidden laptop:flex desktop:hidden"
+            enterFrom="transform -tranlateY-50 opacity-0 max-h-0"
+            enterTo="transform tranlateY-0 opacity-100 max-h-[1000px]"
+            leaveFrom="transform tranlateY-0 opacity-100 max-h-[1000px]"
+            leaveTo="transform -tranlateY-50 opacity-0 max-h-0"
+        >
+            <Container>
+                <Calendar />
+                <Reminder />
+                <Journel />
+                <Thinking />
+            </Container>
+        </Transition>
+        <div className="w-full flex laptop:hidden desktop:flex">
+            <Container>
+                <Calendar />
+                <Reminder />
+                <Journel />
+                <Thinking />
+            </Container>
+        </div>
+    </div>
 }
 
 const Container = styled.div`
