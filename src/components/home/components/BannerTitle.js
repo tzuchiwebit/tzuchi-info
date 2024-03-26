e2e03d5818c91a31c6ad1afce225c5332140b5dd
garry.chiao <garry.chiao@full-stack-artisan.com>
2024-03-26 16:41:39 +0800

    feat(home): banner title modified

--- src/components/home/components/BannerTitle.js
+++ src/components/home/components/BannerTitle.js
@@ -6,15 +6,17 @@ import Icon from "@/shared/Icon"
 export default function BannerTitle({ title = "", link = false }) {
 
     return (
-        <div className="flex flex-row w-full gap-1">
-            <div className="flex-0 text-[26px] font-bold text-primary-blue1 border-solid border-b-2 border-primary-blue1">
+        <div className="flex flex-row w-full gap-2 items-center">
+            <div className="flex-0 text-[26px] font-bold text-primary-blue1 border-solid border-l-[6px] border-primary-blue3 pl-2">
                 {title}
             </div>
-            <div className="flex font-medium justify-end items-end flex-1 text-lg border-solid border-b-2 border-gray-gray7 text-primary-blue3">
-                {
-                    link ? <a href={link} target="_blank" className="flex flex-row whitespace-nowrap">更多<Icon.RightArrow2 width={20} /></a> : <></>
-                }
-            </div>
+            <div className="flex flex-1 text-lg border-solid border-b-2 border-gray-gray7" />
+            {
+                link ? (<div className="flex-0 font-medium justify-end items-end text-lg text-primary-blue3">
+                        <a href={link} target="_blank" className="flex flex-row whitespace-nowrap">更多<Icon.RightArrow2 width={20} /></a>
+                    </div>) : <></>
+            }
+
         </div>
     )
 }
