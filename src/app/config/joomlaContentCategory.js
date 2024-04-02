efc1ee0053618be26bd283b4b35ad56e532f538a
garry.chiao <garry.chiao@full-stack-artisan.com>
2024-04-02 15:01:12 +0800

    feat(chore): routes config and joomla category id config

---
+++ src/app/config/joomlaContentCategory.js
@@ -0,0 +1,58 @@
+export default [
+    {
+        id: 2,
+        name: 'Uncategorised'
+    },
+    {
+        id: 8,
+        name: 'Headline'
+    },
+    {
+        id: 9,
+        name: '選單'
+    },
+    {
+        id: 10,
+        name: '志工早會'
+    },
+    {
+        id: 11,
+        name: '證嚴上人每日一叮嚀'
+    },
+    {
+        id: 14,
+        name: '各據點消息'
+    },
+    {
+        id: 15,
+        name: '社區故事'
+    },
+    {
+        id: 16,
+        name: '專欄文章'
+    },
+    {
+        id: 17,
+        name: '熱門活動'
+    },
+    {
+        id: 18,
+        name: '基金會公告'
+    },
+    {
+        id: 19,
+        name: '好書推薦'
+    },
+    {
+        id: 20,
+        name: 'Youtube小編精選'
+    },
+    {
+        id: 21,
+        name: 'footer'
+    },
+    {
+        id: 22,
+        name: '入口網'
+    },
+]
--- src/app/config/routes.js
+++ src/app/config/routes.js
@@ -2,6 +2,7 @@ export default {
     SEARCH: "/search", // 搜尋頁
     VOLUNTEER_MORNING_MEETING: "/volunteer-morning", // 志工早會
     COLUMN_ARTICLE: "/column-article", // 專欄文章
+    FOUNDATION_ANNOUNCEMENT: "/foundation-announcement", // 專欄文章
     // SEARCH: "search", // 搜尋頁
     // SEARCH: "search", // 搜尋頁
     // SEARCH: "search", // 搜尋頁
