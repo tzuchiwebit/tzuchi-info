3c5933e7ae658bb7b57474de56a734b1ca60b008
garry.chiao <garry.chiao@full-stack-artisan.com>
2024-04-13 07:00:47 +0800

    feat(api): joomla api state published set

--- src/api/joomlaApi.js
+++ src/api/joomlaApi.js
@@ -7,14 +7,14 @@ import joomlaContentCategory from './joomlaContentCategory'
 const API_ENDPOINT = `https://webtest.tzuchi-org.tw/api/index.php/v1/content`
 const token = process.env.NEXT_PUBLIC_JOOMLA_API_TOKEN
 
-const getArticlesByCategory = async (category = '志工早會', limit = 10, offset = 0) => {
+const getArticlesByCategory = async (category = '志工早會', limit = 10, offset = 0, state = 1) => {
     try {
         const targetCategory = _.find(joomlaContentCategory, (i) => i.name === category);
         if (!targetCategory) {
             throw new Error(`Invalid category : ${category}`);
         }
 
-        const res = await axios.get(`${API_ENDPOINT}/articles?filter[category]=${targetCategory.id}&page[limit]=${limit}&page[offset]=${offset}`, {
+        const res = await axios.get(`${API_ENDPOINT}/articles?filter[category]=${targetCategory.id}&page[limit]=${limit}&page[offset]=${offset}&filter[state]=${state}`, {
             headers: {
                 'Authorization': 'Bearer ' + token
             }
