112e4d7fa5926c901a274a211ef1d42e927c7c7d
Bruce Hsu <bruce0205@gmail.com>
2024-05-09 10:26:17 +0800

    style: add link to "NavbarMarquee"

--- src/components/navigation/navbar/components/NavbarMarquee.js
+++ src/components/navigation/navbar/components/NavbarMarquee.js
@@ -7,11 +7,23 @@ import styled from "styled-components";
 
 const NavbarMarquee = () => {
 
+  const openLink = (url) => {
+    window.open(url)
+  }
+
     return (<div className="w-full h-[40px] text-white bg-primary-blue1 flex items-center justify-center">
         <Marquee>
-            <div className="font-bold flex gap-3 items-center">
+            <div className="ml-[20vw] font-bold flex gap-3 items-center">
                 <PrefixDialog>慈濟快報 <span className="absolute -right-[10px] text-secondary-blueGreen">►</span></PrefixDialog>
-                愛心無國界 馳援烏克蘭
+                <Message onClick={()=> openLink('/article/220')}>善愛循環 敘利亞男孩投身慈濟志工</Message>
+            </div>
+            <div className="ml-[20vw] font-bold flex gap-3 items-center">
+                <PrefixDialog>慈濟快報 <span className="absolute -right-[10px] text-secondary-blueGreen">►</span></PrefixDialog>
+                <Message onClick={()=> openLink('/article/1822')}>滿納海敘利亞難民學童 齋戒月行善回饋</Message>
+            </div>
+            <div className="ml-[20vw] font-bold flex gap-3 items-center">
+                <PrefixDialog>慈濟快報 <span className="absolute -right-[10px] text-secondary-blueGreen">►</span></PrefixDialog>
+                <Message onClick={()=> openLink('/article/1834')}>美國慈青、慈少食物發放 加州貧苦家庭見證臺灣愛心</Message>
             </div>
         </Marquee>
     </div>)
@@ -29,4 +41,13 @@ const PrefixDialog = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+`
+
+const Message = styled.div`
+  &:hover {
+    text-decoration-line: underline;
+    text-decoration-style: solid;
+    text-underline-offset: 4px;
+    cursor: pointer;
+  }
 `
