a6c916e3c53dd7ae993a06bca2ca0307a04a7b4d
garry.chiao <garry.chiao@full-stack-artisan.com>
2024-06-21 04:08:45 +0800

    chore: gtm set

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import './globals.css'
 import 'react-loading-skeleton/dist/skeleton.css'
 import Layout from './layout'
 import Head from 'next/head'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function MyApp({ Component, pageProps }) {
     return (
@@ -10,6 +11,7 @@ export default function MyApp({ Component, pageProps }) {
                 <meta charSet="UTF-8" />
                 <link rel="icon" href="./favicon.svg" sizes="any" />
                 <title>慈濟資訊網</title>
+                <GoogleTagManager gtmId="GTM-KD3PM6KH" />
             </Head>
             <Layout>
                 <Component {...pageProps} />
