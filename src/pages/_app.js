d15f3fe1a72f9bb80e277e68639d030e70f9e1c0
garry.chiao <garry.chiao@full-stack-artisan.com>
2024-07-07 23:19:52 +0800

    fix(gtm): gtm fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,10 @@ import './globals.css'
 import 'react-loading-skeleton/dist/skeleton.css'
 import Layout from './layout'
 import Head from 'next/head'
-import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <>
-            <GoogleTagManager gtmId="GTM-KD3PM6KH" />
             <Head>
                 <meta charSet="UTF-8" />
                 <link rel="icon" href="./favicon.svg" sizes="any" />
@@ -17,6 +15,5 @@ export default function MyApp({ Component, pageProps }) {
                 <Component {...pageProps} />
             </Layout>
         </>
-
     )
 }
