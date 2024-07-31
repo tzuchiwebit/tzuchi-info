2a49e0dd59847a1d71458e6b85afa629ec389f43
Bruce Hsu <bruce0205@gmail.com>
2024-07-31 13:27:02 +0800

    fix: layout style

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -4,16 +4,23 @@ import { ThemeProvider } from '@/lib/ThemeProvider'
 import Navbar from '@/components/navigation/navbar/Navbar'
 import Footer from '@/components/navigation/footer/Footer'
 // import Chat from '@/components/navigation/chat/Chat'
+import { useState, useEffect } from "react"
 
 export default function RootLayout({ children }) {
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => {
+    setMounted(true)
+  }, [])
     return (
         <ThemeProvider>
+          {mounted &&
             <StyledComponentsRegistry>
                 <Navbar />
                 {children}
                 <Footer />
                 {/* <Chat /> */}
             </StyledComponentsRegistry>
+            }
         </ThemeProvider>
     )
 }
