be21ce5ec7710486c944e229ed9e5daebfa5ef94
Bruce Hsu <bruce0205@gmail.com>
2024-08-22 14:56:01 +0800

    fix

--- src/api/routeApi.js
+++ src/api/routeApi.js
@@ -1,5 +1,6 @@
 export const getArticleById = async (id) => {
   console.log('SITE_URL', process.env.SITE_URL)
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
   const res = await fetch(`${process.env.SITE_URL}/api/article?id=${id}`, {
     headers: {
       'Content-Type': 'application/json',
