e05fc4275cc01691a2695d53755bcf2f3e0901ea
Bruce Hsu <bruce0205@gmail.com>
2024-08-22 14:49:44 +0800

    modify

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -6,8 +6,8 @@ export async function generateMetadata({ params, searchParams}, parent) {
 
   return {
     metadataBase: new URL(`${process.env.SITE_URL}/search`),
-    title: `${searchParams.keyword} 搜尋結果 - 慈濟資訊網`,
-    description: `以下是含有 ${searchParams.keyword} 的搜尋結果。`,
+    title: `${searchParams?.keyword}搜尋結果 - 慈濟資訊網`,
+    description: `以下是含有 ${searchParams?.keyword} 的搜尋結果。`,
     openGraph: {
       images: ['https://imagedelivery.net/oK0RK5YvW3bVFXgaGP6foQ/032741ee-fac7-44b6-3cea-649da4b8ff00/2K'],
     },
