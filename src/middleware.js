872cc45deeb3ea42287de50ee61888f7c551e860
Bruce Hsu <bruce0205@gmail.com>
2024-11-08 14:22:11 +0800

    modify

--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,37 @@
-// middleware.js
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
+import { getArticleById } from "@/api/routeApi";
+import { validateArticle } from "@/utils";
 
-export function middleware(request) {
-    const requestHeaders = new Headers(request.headers);
-    requestHeaders.set('x-url', request.url);
+export const config = {
+  matcher: ["/article/:path*"],
+};
 
-    return NextResponse.next({
-        request: {
-            // Apply new request headers
-            headers: requestHeaders,
-        }
-    });
+async function articleMiddleware(id) {
+  const article = (await getArticleById(id)).data;
+  console.log("articleMiddleware", id)
+  if (
+    !validateArticle({
+      state: article?.attributes?.state,
+      publishUp: article?.attributes?.publish_up,
+      publishDown: article?.attributes?.publish_down,
+    })
+  ) {
+    console.log("invalidate article");
+    return Response.redirect(process.env.NEXT_PUBLIC_URL, 301);
+  }
+}
+
+export async function middleware(request) {
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/article")) {
+    const match = pathname.match(/\/article\/(\d+)/);
+    if (match) {
+      const id = match[1];
+      await articleMiddleware(id);
+    }
+  }
+
+  // Continue with the request
+  return NextResponse.next();
 }
