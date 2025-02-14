ec487efd9886b99199870177a3e6111fa9790e86
Bruce Hsu <bruce0205@gmail.com>
2025-02-14 15:14:12 +0800

    modify: add "全球聯絡點" link to footer

--- src/shared/Icon/Icon.js
+++ src/shared/Icon/Icon.js
@@ -14,7 +14,7 @@ export { default as UpArrow } from "./icon/main/up-arrow.svg";
 export { default as Like } from "./icon/main/like.svg";
 export { default as LikeFull } from "./icon/main/like-full.svg";
 export { default as File } from "./icon/main/file.svg";
-export { default as Link } from "./icon/main/link.svg";
+export { default as LinkOut } from "./icon/main/link-out.svg";
 export { default as PaginationDoubleLeftArrow } from "./icon/main/pagination-double-left-arrow.svg";
 export { default as PaginationDoubleRightArrow } from "./icon/main/pagination-double-right-arrow.svg";
 export { default as PaginationLeftArrow } from "./icon/main/pagination-left-arrow.svg";
