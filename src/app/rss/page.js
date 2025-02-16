9dc2de368a332c069a19f840719a0289fb55569b
Bruce Hsu <bruce0205@gmail.com>
2025-02-16 12:44:22 +0800

    omit

--- src/app/rss/page.js
+++ src/app/rss/page.js
@@ -4,8 +4,8 @@ import { Suspense } from "react";
 export async function generateMetadata({ params }, parent) {
   return {
     metadataBase: new URL(`${process.env.NEXT_PUBLIC_URL}/volunteer-morning`),
-    title: `上人開示 - 慈濟資訊網`,
-    description: `證嚴上人每天在志工早會的開示，是慈濟志工每天必須攝取的資糧；在開示中，上人觀機施教，藉事顯理，指引弟子與大眾行走人間菩薩道。`,
+    title: `RSS - 慈濟資訊網`,
+    // description: `證嚴上人每天在志工早會的開示，是慈濟志工每天必須攝取的資糧；在開示中，上人觀機施教，藉事顯理，指引弟子與大眾行走人間菩薩道。`,
     openGraph: {
       images: ['https://imagedelivery.net/oK0RK5YvW3bVFXgaGP6foQ/032741ee-fac7-44b6-3cea-649da4b8ff00/2K'],
     },
