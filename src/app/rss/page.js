e2015ff6f48a34ce6b2e0cd03dd070b3ca4d12eb
Bruce Hsu <bruce0205@gmail.com>
2025-02-27 15:30:07 +0800

    modify: add metadata to "rss" page

--- src/app/rss/page.js
+++ src/app/rss/page.js
@@ -4,8 +4,8 @@ import { Suspense } from "react";
 export async function generateMetadata({ params }, parent) {
   return {
     metadataBase: new URL(`${process.env.NEXT_PUBLIC_URL}/volunteer-morning`),
-    title: `RSS - 慈濟資訊網`,
-    // description: `證嚴上人每天在志工早會的開示，是慈濟志工每天必須攝取的資糧；在開示中，上人觀機施教，藉事顯理，指引弟子與大眾行走人間菩薩道。`,
+    title: `RSS 頻道 - 慈濟資訊網`,
+    description: `歡迎訂閱慈濟資訊網 RSS 頻道！透過 RSS，您可以即時獲取慈濟最新的公益活動、人文故事、慈善項目及相關訊息。無需逐一瀏覽網站，只需將我們的 RSS 連結加入您的閱讀器，即可隨時掌握慈濟全球愛心行動的動態，與我們一同見證善的力量。`,
     openGraph: {
       images: ['https://imagedelivery.net/oK0RK5YvW3bVFXgaGP6foQ/032741ee-fac7-44b6-3cea-649da4b8ff00/2K'],
     },
