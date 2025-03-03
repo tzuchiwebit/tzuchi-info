b2bfe9d64d6e55f332fb387d857f2f3977c03afa
Bruce Hsu <bruce0205@gmail.com>
2025-03-03 16:11:14 +0800

    fix

--- src/app/master-talk/client.js
+++ src/app/master-talk/client.js
@@ -38,11 +38,12 @@ export default function Page() {
   useEffect(() => {
     // 只對這個頁面禁用滾動位置記憶
     const originalScrollRestoration = window.history.scrollRestoration;
+    console.log("originalScrollRestoration", originalScrollRestoration)
     window.history.scrollRestoration = "manual";
 
     return () => {
       // 當離開此頁面時，恢復原本的滾動行為
-      window.history.scrollRestoration = "auto";
+      window.history.scrollRestoration = originalScrollRestoration;
     };
   }, []);
 
